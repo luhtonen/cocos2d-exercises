44435552d61836055ad4aa230751c355b42bf14f
Eduard Luhtonen <luhtonen@gmail.com>
2012-09-16 11:20:04 +0300

    switched to use LayerColor super class

--- NEW_COCOS2D-HTML5_APP/NEW_COCOS2D-HTML5_APP.js
+++ NEW_COCOS2D-HTML5_APP/NEW_COCOS2D-HTML5_APP.js
@@ -1,14 +1,12 @@
-var NEW_COCOS2D-HTML5_APP = cc.Layer.extend({
+var NEW_COCOS2D-HTML5_APP = cc.LayerColor.extend({
 	init : function() {
 		this._super();
-
-		var winSize = cc.Director.getInstance().getWinSize();
-		var mainLayer = cc.LayerColor.create(new cc.Color4B(255, 255, 0, 255),
-				600, 600);
+		this.initWithColor(new cc.Color4B(255, 255, 0, 255));
+		var size = cc.Director.getInstance().getWinSize();
 
 		/* Create HelloWorld label and add it to the main layer */
 		var helloLabel = cc.LabelTTF.create("Hello world", "Arial", 30);
-		helloLabel.setPosition(new cc.Point(winSize.width / 2, winSize.height / 2));
+		helloLabel.setPosition(new cc.Point(size.width / 2, size.height / 2));
 		helloLabel.setColor(new cc.Color3B(255, 0, 0));
 		var rotationAmount = 0;
 		var scale = 1;
@@ -21,10 +19,9 @@ var NEW_COCOS2D-HTML5_APP = cc.Layer.extend({
 			if (scale > 10)
 				scale = 1;
 		});
-		mainLayer.addChild(helloLabel);
+		this.addChild(helloLabel);
 		
-		this.addChild(mainLayer);
-		return true;
+		return this;
 	}
 });
 
